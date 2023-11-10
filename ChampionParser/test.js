20a1b430af7afdab62cd789c9bb3ab1e1d65dc1a
SanchoPansa <zelgadis4@gmail.com>
2023-11-11 00:45:09 +0300

    Unit Testing

--- ChampionParser/test.js
+++ ChampionParser/test.js
@@ -1,12 +1,22 @@
+import { describe } from "mocha";
 import jsonToLua from "../JsonToLua.js";
+import { assert } from "chai";
 
-let a = {
-  a: "5",
-  b: [true, false],
-  c: {
-    cc: "%",
-    cd: 54
-  }
-}
+describe("Преобразователь объектов JS в таблицу Lua", function() {
+  let obj = {
+    a: 5,
+    b: "c"
+  };
 
-console.log(jsonToLua(a));
+  it("Должен превратить простой литерал объекта в таблицу Lua", function() {
+    assert.strictEqual(jsonToLua(obj), `\t\t["a"] = 5,\n\t["b"] = "c"`);
+  });
+
+  it("Должен изменить название ключа в соответствии с переменной 'replacer'", function() {
+    assert.strictEqual(jsonToLua(obj, {b: "beta"}), `\t\t["a"] = 5,\n\t["beta"] = "c"`);
+  });
+
+  it("Должен исключить переменную в соответствии с массивом 'exclude'", function() {
+    assert.strictEqual(jsonToLua(obj, {}, ["a"]), `\t\t["b"] = "c"`);
+  });
+})
