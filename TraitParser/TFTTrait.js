92fcf851bc39ceb7560167bc5d811782952ad1b7
SanchoPansa <zelgadis4@gmail.com>
2023-11-14 00:59:52 +0300

    New Trait class structure

--- TraitParser/TFTTrait.js
+++ TraitParser/TFTTrait.js
@@ -1,10 +1,23 @@
+/**
+ * Данный класс описывает особенность чемпиона TFT
+ */
 export default class Trait {
+  /**
+   * 
+   * @param {String} name Имя группы
+   * @param {String} engname Английское имя группы
+   * @param {String} synergy Основное описание эффекта комбинации
+   * @param {[Number, String][]} levels Двумерный массив с гетерогенным массивом; первое значение - число бойцов комбинации, второе - описание эффекта
+   * @param {Boolean} prismaticLevel Имеет ли призматический уровень
+   */
   constructor(name, engname, synergy, levels, prismaticLevel) {
     this.name = name;
     this.engname = engname;
-    this.icon = engname + " TFT9 icon.png";
+    this.gs = gs;
+    this.gp = gp;
+    this.icon = icon;
     this.synergy = synergy;
-    this.levels = levels;
     this.prismaticLevel = prismaticLevel;
+    this.levels = levels;
   }
 }
